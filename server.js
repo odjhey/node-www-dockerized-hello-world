6a6ebd0934042d0966aa24eab6a217d3416b97bf
odz <odjhey@gmail.com>
2021-05-03 01:15:48 +0800

    update message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.get("/", function (req, res) {
-  return res.send("hello brotha!");
+  return res.send("yaharu!");
 });
 
 app.listen(port, (err) => {
